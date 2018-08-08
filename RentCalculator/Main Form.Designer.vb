9da9a13883398b7c1576b48518b21f55ba4ef8d5
Eric Patrick <erstpa@gmail.com>
2018-08-08 13:28:10 -0400

    Completed UI

---
+++ RentCalculator/Main Form.Designer.vb
@@ -0,0 +1,316 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtPower = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtWater = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtInternet = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtStorage = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtTelevision = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtExpenses1 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtRent = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtExpenses2 = New System.Windows.Forms.TextBox()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.lblTotal1 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.lblTotal2 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.lblTotalOwed = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(49, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "FPL:"
+        '
+        'txtPower
+        '
+        Me.txtPower.Location = New System.Drawing.Point(83, 12)
+        Me.txtPower.Name = "txtPower"
+        Me.txtPower.Size = New System.Drawing.Size(100, 23)
+        Me.txtPower.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(46, 44)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "City:"
+        '
+        'txtWater
+        '
+        Me.txtWater.Location = New System.Drawing.Point(83, 41)
+        Me.txtWater.Name = "txtWater"
+        Me.txtWater.Size = New System.Drawing.Size(100, 23)
+        Me.txtWater.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(20, 73)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 15)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Comcast:"
+        '
+        'txtInternet
+        '
+        Me.txtInternet.Location = New System.Drawing.Point(83, 70)
+        Me.txtInternet.Name = "txtInternet"
+        Me.txtInternet.Size = New System.Drawing.Size(100, 23)
+        Me.txtInternet.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(27, 105)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 15)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Storage:"
+        '
+        'txtStorage
+        '
+        Me.txtStorage.Location = New System.Drawing.Point(83, 102)
+        Me.txtStorage.Name = "txtStorage"
+        Me.txtStorage.Size = New System.Drawing.Size(100, 23)
+        Me.txtStorage.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(26, 134)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(51, 15)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "DirecTV:"
+        '
+        'txtTelevision
+        '
+        Me.txtTelevision.Location = New System.Drawing.Point(83, 131)
+        Me.txtTelevision.Name = "txtTelevision"
+        Me.txtTelevision.Size = New System.Drawing.Size(100, 23)
+        Me.txtTelevision.TabIndex = 9
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(20, 163)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(57, 15)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Expenses:"
+        '
+        'txtExpenses1
+        '
+        Me.txtExpenses1.Location = New System.Drawing.Point(83, 160)
+        Me.txtExpenses1.Name = "txtExpenses1"
+        Me.txtExpenses1.Size = New System.Drawing.Size(100, 23)
+        Me.txtExpenses1.TabIndex = 11
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(261, 15)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(34, 15)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Rent:"
+        '
+        'txtRent
+        '
+        Me.txtRent.Location = New System.Drawing.Point(301, 12)
+        Me.txtRent.Name = "txtRent"
+        Me.txtRent.Size = New System.Drawing.Size(100, 23)
+        Me.txtRent.TabIndex = 13
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(238, 44)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(57, 15)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "Expenses:"
+        '
+        'txtExpenses2
+        '
+        Me.txtExpenses2.Location = New System.Drawing.Point(301, 41)
+        Me.txtExpenses2.Name = "txtExpenses2"
+        Me.txtExpenses2.Size = New System.Drawing.Size(100, 23)
+        Me.txtExpenses2.TabIndex = 15
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(287, 159)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalc.TabIndex = 16
+        Me.btnCalc.Text = "&Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(287, 188)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 17
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(41, 193)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(36, 15)
+        Me.Label9.TabIndex = 18
+        Me.Label9.Text = "Total:"
+        '
+        'lblTotal1
+        '
+        Me.lblTotal1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotal1.Location = New System.Drawing.Point(83, 189)
+        Me.lblTotal1.Name = "lblTotal1"
+        Me.lblTotal1.Size = New System.Drawing.Size(100, 23)
+        Me.lblTotal1.TabIndex = 19
+        Me.lblTotal1.Text = "Label10"
+        Me.lblTotal1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(259, 74)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(36, 15)
+        Me.Label11.TabIndex = 20
+        Me.Label11.Text = "Total:"
+        '
+        'lblTotal2
+        '
+        Me.lblTotal2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotal2.Location = New System.Drawing.Point(301, 70)
+        Me.lblTotal2.Name = "lblTotal2"
+        Me.lblTotal2.Size = New System.Drawing.Size(100, 23)
+        Me.lblTotal2.TabIndex = 21
+        Me.lblTotal2.Text = "Label12"
+        Me.lblTotal2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(227, 104)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(68, 15)
+        Me.Label13.TabIndex = 22
+        Me.Label13.Text = "Total owed:"
+        '
+        'lblTotalOwed
+        '
+        Me.lblTotalOwed.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotalOwed.Location = New System.Drawing.Point(301, 100)
+        Me.lblTotalOwed.Name = "lblTotalOwed"
+        Me.lblTotalOwed.Size = New System.Drawing.Size(100, 23)
+        Me.lblTotalOwed.TabIndex = 23
+        Me.lblTotalOwed.Text = "Label14"
+        Me.lblTotalOwed.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(933, 519)
+        Me.Controls.Add(Me.lblTotalOwed)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.lblTotal2)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.lblTotal1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.txtExpenses2)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txtRent)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtExpenses1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtTelevision)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtStorage)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtInternet)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtWater)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtPower)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "frmMain"
+        Me.Text = "Rent Fee Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtPower As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtWater As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtInternet As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtStorage As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtTelevision As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtExpenses1 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtRent As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtExpenses2 As TextBox
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label9 As Label
+    Friend WithEvents lblTotal1 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents lblTotal2 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents lblTotalOwed As Label
+End Class
--- RentCalculator/My Project/Application.Designer.vb
+++ RentCalculator/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.RentCalculator.Form1
+            Me.MainForm = Global.RentCalculator.frmMain
         End Sub
     End Class
 End Namespace
